1d1e2ed54fa76213028fe61018727cdfc822b8b9
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-05-24 16:44:20 +0200

    jenkins: do not hide build log

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,3 +1,5 @@
 opam pin deriving .
-opam install type_conv deriving
-opam remove deriving
+opam install type_conv
+opam install --deps-only deriving
+opam install --verbose deriving
+opam remove --verbose deriving
